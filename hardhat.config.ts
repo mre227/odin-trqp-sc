b035f919ccdb6f4e789483a4588f95803f7847a3
Aziret-Ali Ismailov <ismailovisaac@gmail.com>
2026-04-08 22:47:24 -0400

    build: upgrade solc to 0.8.24 and enable viaIR with size-focused optimizer settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,24 @@ import "@nomiclabs/hardhat-ethers";
 import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.0"
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+      debug: {
+        revertStrings: "strip",
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
 };
 
 export default config;
